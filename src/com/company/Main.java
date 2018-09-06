ae80e041782f38cdf9c1e2a6b95b48bc035bb12c
twildasin <tjwild127@aol.com>
2018-09-05 22:09:36 -0400

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         
-        System.out.println("");
+        System.out.println("\n79 + 3 * (4 + 82 - 68) - 7 + 19 = " + (79 + 3 * (4 + 82 - 68) - 7 + 19));
+        System.out.println("\n(179 + 21 + 10) / 7 + 181 = " + ((179 + 21 + 10) / 7 + 181));
+        System.out.println("\n10389 * 56 * 11 + 2246 = " + (10389 * 56 * 11 + 2246));
+
 
     }
 }
